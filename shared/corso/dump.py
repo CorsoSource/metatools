28e335fa181bb82979892392a0241c63bbedc2d9
Andrew Geiger <andrew.geiger@corsosystems.com>
2019-08-12 20:06:41 -0400

    Fixed the deserialization - Windows were not getting decoded correctly.

--- shared/corso/dump.py
+++ shared/corso/dump.py
@@ -2,14 +2,51 @@
 	Plonk objects to disk!
 """
 
-import array.array, base64
+import array.array, base64, os
 from shared.corso.meta import getDesignerContext
 
 
 __all__ = ['getResourceXML']
 
 
-def getResourceXML(resourcePath = None, anchor=None):
+def serializeToXML(obj, anchor=None):
+	serializer = getSerializer(anchor)
+	serializer.addObject(obj)
+	return serializer.serializeXML()
+
+def getDeserializer(anchor = None):
+	context = getDesignerContext(anchor)
+	return context.createDeserializer()
+
+def getSerializer(anchor = None):
+	context = getDesignerContext(anchor)
+	return context.createSerializer()
+	
+
+def resolveToObject(objBinary, failsafe=5):
+	if not failsafe:
+		return NotImplemented
+	objects = []
+	
+	deserializerContext = getDeserializer().deserialize(objBinary)
+	rootObjects = deserializerContext.getRootObjects()
+	
+	for obj in rootObjects:
+		if isinstance(obj, (str, unicode)):
+			objects.append(obj)
+		elif str(type(obj)).startswith("<type 'com.inductiveautomation.factorypmi.application.model"):
+			if 'getSerializedBytes' in dir(obj):
+				objects.extend(resolveToObject(obj.getSerializedBytes(), failsafe-1))
+			elif 'getSerializedCode' in dir(obj):
+				objects.extend(resolveToObject(obj.getSerializedCode(), failsafe-1))
+			else:
+				pass #???!
+		else:
+			objects.append(obj)
+	return objects
+
+
+def getResource(resourcePath = None, anchor=None):
 	"""Given a resource path, this will return the human-readable
 	text XML version of that resource.
 	
@@ -19,9 +56,6 @@ def getResourceXML(resourcePath = None, anchor=None):
 	"""	
 	context = getDesignerContext(anchor)
 	
-	deserializer = context.createDeserializer()
-	serializer = context.createSerializer()
-	
 	proj = context.getProject()
 	
 	if resourcePath:	
@@ -30,30 +64,63 @@ def getResourceXML(resourcePath = None, anchor=None):
 		resources = [res for res in proj.getResources()]
 	
 	resourceData = {}
+	errors = []
 	for resource in resources:
 		if resource is None:
 			continue # no data retrieved...
 			
 		for dataKey in resource.getDataKeys():
 			
-			resDataKey = '%s :: %s' % (resource.resourcePath, dataKey)
-			
+			resDataKey = '%s--%s' % (resource.resourcePath, dataKey)
 			resData = resource.getData(dataKey)
 			
-			if all((dataKey.endswith('bin'), 
+			if dataKey.endswith('.py'):
+				resourceData[resDataKey] = [''.join([chr(c) for c in resData])]
+			elif all((dataKey.endswith('bin'), 
 					isinstance(resData, array.array), 
 					resData.typecode == 'b')):
-					
-				deserializerContext = deserializer.deserialize(resData)
-				rootObjects = deserializerContext.getRootObjects()
-
-				resourceData[resDataKey] = [ro for ro in rootObjects]
-		
-				for ro in rootObjects:
-					serializer.addObject(ro)
+				try:
+					objs = resolveToObject(resData)
+					resourceData[resDataKey] = [obj for obj in objs if not obj is NotImplemented]
+				except SerializationException:
+					errors.append(resDataKey)
 			else:
-				resourceData[resDataKey] = base64.b64encode(resData)
-				
-	xmlOut = serializer.serializeXML()
+				resourceData[resDataKey] = [base64.b64encode(resData)]
 	
-	return xmlOut
+	print 'Errors with de/serialization:\n%s' % '\n'.join(errors)
+	
+	return resourceData
+
+
+def dumpProject(dumpDir):
+
+	resourceData = getResource()
+	for dataKey,objects in resourceData.items():
+		for i,obj in enumerate(objects):
+			
+			name,_,dataSig = dataKey.rpartition('--')
+			dataType,_,dataExt = dataSig.rpartition('.')
+			if dataExt == 'bin': 
+				dumpString = serializeToXML(obj)
+				dataExt = 'xml'
+			else:
+				dumpString = obj
+		
+			if i:
+				filepath = '%s/%s_%d.%s' % (dumpDir, name, i, dataExt)
+			else:
+				filepath = '%s/%s.%s' % (dumpDir, name, dataExt)
+			
+			if os.path.exists(filepath):
+				if i:
+					filepath = '%s/%s_%d.%s' % (dumpDir, dataKey, i, dataExt)
+				else:
+					filepath = '%s/%s.%s' % (dumpDir, dataKey, dataExt)
+					
+			try:
+				os.makedirs(filepath.rpartition('/')[0])
+			except OSError:
+				pass
+				
+			with open(filepath, 'w') as f:			
+				f.write(dumpString)
