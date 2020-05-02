896be0a4906dbfdac9e1ef3435f9d6f5e1aef933
Andrew Geiger <andrew.geiger@corsosystems.com>
2020-05-02 00:33:11 -0400

    Adding command scaffolds

--- shared/tools/tracer.py
+++ shared/tools/tracer.py
@@ -32,7 +32,7 @@ running_thread = monitored()
 
 
 from copy import deepcopy
-# from shared.tools.global import ExtraGlobal
+#from shared.tools.global import ExtraGlobal
 
 from StringIO import StringIO
 
@@ -362,12 +362,17 @@ class Tracer(object):
 	For more information and the cool implementation that we're tweaking here,
 	  see also rpdb at https://github.com/tamentis/rpdb
 	"""
-	__slots__ = ('thread', 'thread_state', 'active',
-				 'sys',
-				 'context_buffer', '_dispatch_mapping',
-				 'command', 'monitoring', 'intercepting',
-				 '_shutdown', '_monitoring_thread',
-				 '_debug',
+	__slots__ = ('thread', 'thread_state', 'sys',
+				 'monitoring', 'intercepting',
+				 'context_buffer', 
+				 '_map_for_dispatch', '_map_o_commands', 
+
+				 '_pending_commands', '_command', '_alias_commands',
+
+				 '_bottom_frame',
+
+				 '_debug', '_cursor_frame', # DeprecationWarning
+				 
 			#####	 'monitoring', 'tracing',
 				)
 
@@ -387,49 +392,92 @@ class Tracer(object):
 			
 	def __init__(self, thread=None):
 		
-		self._shutdown = False
 		self.monitoring = False
 		self.intercepting = False
-		self.command = ''
-		self._debug = {}
-		
-		self._dispatch_mapping = dict(
+		self.context_buffer = deque()
+
+		self._map_for_dispatch = dict(
 			(event, getattr(self, 'dispatch_%s' % event))
 			for event in self._event_labels)
+			
+		self._map_o_commands = dict(
+			(command, getattr(self, command_method))
+			for command_method in dir(self)
+			if attribute.startswith('_command_'))
 		
+		self.command = ''
+		self._alias_commands = {}
+		self._pending_commands = []
+
+		self._bottom_frame = None
+
+		self._debug = {}
+		self._cursor_frame = None
+
+
 		self.thread = thread
 		self.thread_state = getThreadState(thread)
 		
-		self.context_buffer = deque()
-
 		self.sys = SysHijack(thread)
 		
 		#start the machinery so we can inject directly
 		self.sys.settrace(self._nop)
 		
 		self._active_tracers[thread] = self
-		
+
 
 	# Context control - start and stop active tracing
 	def __enter__(self):
-		self.monitoring = True
-		self.sys._getframe().f_trace = self.dispatch
-		#self.sys.settrace(self.dispatch)
+		self.monitor()
 		return self
 
 	def __exit__(self, exc_type, exc_val, exc_tb):
-		self.monitoring = False
+		self.shutdown()
+
+	# Install and Uninstall
+	def _stack_install(self):
+		"""Install the trace dispatcher to every level of the stack 
+		and then set the trace machinery in motion.
+		"""
+		frame = self.sys._getframe()
+		while frame:
+			frame.f_trace = self.dispatch
+			# track the furthest down the stack goes
+			self._bottom_frame = frame
+			frame = frame.f_back
+		self.sys.settrace(self.dispatch)
+
+	def _stack_uninstall(self):
+		"""Turn off trace and remove the trace dispatcher from every level 
+		in the stack.
+		"""
 		self.sys.settrace(None)
+		frame = self.sys._getframe()
+		while frame and frame is not self._bottom_frame:
+			del frame.f_trace
+			frame = frame.f_back
+
+	# Run state status controls
+	def monitor(self):
+		"""Begin trace, watching each frame event."""
+		self.monitoring = True
+		self._stack_install()
+	
+		self._debug['entry'] = self.sys._getframe()		
 
 	def intercept(self):
+		"""Begin trace, intercepting each frame event and waiting for commands."""
 		self.intercepting = True
-		_ = self.__enter__()
-
+		self.monitor()
+		
+		
 	def shutdown(self):
+		"""Stop the trace and tear down the setup."""
 		self.intercepting = False
 		self.monitoring = False
-		self._shutdown = True
+		self._stack_uninstall()
 
+	# Convenience functions
 	@property
 	def current_locals(self):
 		return self.sys._getframe().f_locals
@@ -437,6 +485,10 @@ class Tracer(object):
 	@property
 	def current_globals(self):
 		return self.sys._getframe().f_globals
+		
+	@property
+	def current_frame(self):
+		return self.sys._getframe()
 
 	# History controls
 
@@ -444,18 +496,7 @@ class Tracer(object):
 		self.context_buffer.append(context)
 		while len(self.context_buffer) > self.CONTEXT_BUFFER_LIMIT:
 			_ = self.context_buffer.popleft()
-
-
-	# Interaction
-	
-	def _await_input(self):
-	
-		while not self.command:
-			sleep(self._UPDATE_CHECK_DELAY)
-			
-		system.util.getLogger('Tracer').info('Command: %s' % self.command)
-		self.command = ''
-
+		
 
 	# Dispatch
 
@@ -463,9 +504,10 @@ class Tracer(object):
 		if self.monitoring:
 			# Check if execution should be intercepted for debugging
 			if self.intercept_context(frame, event, arg):
-				self._dispatch_mapping.get(event, self._nop)(frame, arg)
+				self._map_for_dispatch.get(event, self._nop)(frame, arg)
 
-				self._await_input()
+				self.await_comand()
+				
 			return self.dispatch
 		else:
 			return # none
@@ -484,8 +526,8 @@ class Tracer(object):
 		if self.intercepting:
 			return True # if already intercepting, continue
 
-#		if frame in self.stop_frames:
-#			return True
+		# if frame in self.stop_frames:
+		# 	return True
 
 		return False 
 
@@ -508,8 +550,222 @@ class Tracer(object):
 		pass
 
 
+	# Interaction
+	
+	def await_comand(self):
+	
+		while not self._pending_commands and self.intercepting:
+			sleep(self._UPDATE_CHECK_DELAY)
+		
+		while self._pending_commands:
+			system.util.getLogger('Tracer').info('Command: %s' % self.command)
+			self.command(self._pending_commands.pop())	
 
 
+	def command(self, command):
+		"""Run a command or execute a statement.
+		
+		Commands designed to be compatible with PDB docs
+		https://docs.python.org/2.7/library/pdb.html?highlight=pdb#debugger-commands
+		"""
+		if not command:
+			return
+
+		if command.ltrim()[0] == '!':
+			self._command_statement(command)
+		else:
+			args = []
+			for arg in command.split():
+				try:
+					args.append(literal_eval(arg))
+				except ValueError:
+					args.append(arg)
+			self._map_o_commands.get(args[0], self._command_default)(command, *args)
+
+
+	# Meta commands
+
+	def _command_help(self):
+		"""Print available commands. If given a command print the command data."""
+		raise NotImplementedError
+	_command_h = _command_help
+
+	def _command_where(self):
+		"""Print a stack trace, with the most recent frame at the bottom, pointing to cursor frame."""
+		raise NotImplementedError
+	_command_w = _command_where 
+
+	def _command_down(self):
+		"""Move the cursor to a more recent frame (down the stack)"""
+	_command_d = _command_down
+
+	def _command_up(self):
+		"""Move the cursor to a more recent frame (up the stack)"""
+	_command_u = _command_up
+
+
+	# Breakpoint controls
+
+	def _command_clear(self, command, *breakpoints):
+		"""Clear breakpoint(s).
+
+		Breakpoints can be by ID or filename:line number.
+
+		If none are provided, clear all after confirmation. (Pulled off _pending_commands)
+		"""
+		raise NotImplementedError
+	_command_cl = _command_clear
+
+	def _command_enable(self, command, *breakpoints):
+		"""Enable the breakpoints"""
+		raise NotImplementedError
+	def _command_disable(self, command, *breakpoints):
+		"""Disable the breakpoints"""
+		raise NotImplementedError
+	def _command_ignore(self, command, breakpoint, num_passes=0):
+		"""Run past breakpoint num_passes times. 
+		Once count goes to zero the breakpoint activates."""
+		raise NotImplementedError
+
+	def _command_break(self, command, stop_location='', stop_condition=lambda:True):
+		"""Create a breakpoint at the given location.
+		
+		The stop_location can be
+		 - a line in the current file
+		 - a function in the current file
+		 - a filename and line (i.e "shared.tools.debug:45")
+		 - a filename and a function (i.e. "shared.tools.meta:getFunctionSigs")
+
+		If a condition is provided, then the breakpoint will be ignored until true.
+
+		If no location or condition is provided, then list all breakpoints:
+		 - breakpoint location and ID
+		 - number of times hit
+		 - remaining ignore count
+		 - conditions, if any
+		 - is temporary
+		"""
+		raise NotImplementedError
+
+	def _command_tbreak(self, command, stop_location='', stop_condition=lambda:True):
+		"""Create a temporary breakpoint at the given location. Same usage otherwise as break."""
+		raise NotImplementedError
+
+	def _command_condition(self, command, breakpoint, condition):
+		"""Stop on breakpoint if condition is True"""
+		raise NotImplementedError
+
+	def _command_commands(self, command, breakpoint):
+		"""Run commands when breakpoint is reached. 
+		Commands entered will be assigned to this breakpoint until 'end' is seen.
+
+		To clear a breakpoint's commands, enter this mode and enter 'end' immediately.
+
+		Any command that resumes execution will prematurely end command list execution.
+		  (This is continue, step, next, return, jump, quit)
+
+		Entering 'silent' will run the breakpoint commands but not stop.
+		"""
+		raise NotImplementedError
+
+	# Execution control
+
+	def _command_step(self, command):
+		"""Step into the next function in the current line (or to the next line, if done)."""
+		raise NotImplementedError
+	_command_s = _command_step
+
+	def _command_next(self, command):
+		"""Continue to the next line (or return statement). 
+		Note step 'steps into a line' (possibly making the call stack deeper)
+		  while next goes to the 'next line in this frame'. 
+		"""
+		raise NotImplementedError
+	_command_n = _command_next
+
+	def _command_until(self, command, target_line=0):
+		"""Continue until a higher line number is reached, or optionally target_line."""
+		raise NotImplementedError
+	_command_u = _command_until
+
+	def _command_return(self, command):
+		"""Continue until the current frame returns."""
+		raise NotImplementedError
+	_command_r = _command_return
+
+	def _command_continue(self, command):
+		"""Resume execution until a breakpoint is reached."""
+		raise NotImplementedError
+	_command_c = _command_cont = _command_continue
+
+	def _command_jump(self, command, target_line):
+		"""Set the next line to be executed. Only possible in the bottom frame.
+		
+		Use this to re-run code or skip code in the current frame.
+		NOTE: You can note jump into the middle of a for loop or out of a finally clause.
+		"""
+		raise NotImplementedError
+	_command_j = _command_jump
+
+
+	# Info commands
+
+	def _command_list(self, command, first=0, last=0):
+		"""List the source code for the current file, +/- 5 lines.
+		Given just first, show code +/- 5 lines around first.
+		Given first and last show code between the two given line numbers.
+		If last is less than first it goes last lines past the first. 
+		"""
+		raise NotImplementedError
+	_command_l = _command_list
+
+	def _command_args(self, command):
+		"""Show the argument list to this function."""
+		raise NotImplementedError
+	_command_a = _command_args
+
+	def _command_p(self, command, expression):
+		"""Print the expression."""
+		raise NotImplementedError
+
+	def _command_pp(self, command, expression):
+		"""Print the expression via pretty printing. This is actually how p works, too."""
+		raise NotImplementedError
+
+	def _command_alias(self, command, name='', command=''):
+		"""Create an alias called name that executes comand. 
+		If no command, then show what alias is. If no name, then show all aliases.
+
+		NOTE: The command should not be in quotes.
+
+		Parameters can be indicated by %1, %2, and so on, while %* is replaced by all parameters.
+		"""
+		raise NotImplementedError
+
+	def _command_unalias(self, command, name):
+		"""Delete the specified alias."""
+		raise NotImplementedError
+
+	def _command_statement(self, raw_command, *tokens):
+		"""Execute the (one-line) statement. 
+		Start the statement with '!' if it starts with a command.
+
+		To set a global variable, prefix the assignment command with `global`
+		  (IPDB) global list_options; list_options['-1']
+		"""
+		# remove '!' or 'statement' from start first!
+		raise NotImplementedError
+	_command_default = _command_bang = _command_statement
+
+	def _command_run(self, command, *args):
+		"""Restart the debugged program. This is not and will not be implemented."""
+		raise NotImplementedError("IPDB will not implement the 'run' command.")
+	
+	def _command_quit(self, command):
+		"""Quit the debugger. Unlike in PDB, this will _not_ kill the program thread."""
+		self.shutdown()
+		self.sys._restore()
+	_command_shutdown = _command_die = _command_q = _command_quit
 
 
 class Trap(object):
