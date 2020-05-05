9bc4942ecc7fc9133fbf61a4fd5005c76b6bf910
Andrew Geiger <andrew.geiger@corsosystems.com>
2020-05-05 12:13:56 -0400

    Caching source code mechanism (since `linecache` doesn't work...)

--- shared/tools/debug/codecache.py
+++ shared/tools/debug/codecache.py
@@ -1,4 +1,15 @@
 
+from functools import wraps
+
+
+def cached(function):
+	@wraps(function)
+	def check_cache_first(cls, *args):
+		if not args in cls._cache:
+			cls._cache[args] = function(cls, *args)
+		return cls._cache[args]
+
+
 
 class CodeCache(object):
 	"""Similar to the linecache module in purpose, but makes sense of the Ignition environment.
@@ -19,14 +30,33 @@ class CodeCache(object):
 	_cache = {}
 
 
-
 	def _dispatch_location(location, frame):
 		"""Resolve and make sense of the location given. 
 
 		It may be "module:shared.tools.debug.codecache" or perhaps 
 		  a vague "event:actionPerformed". This function will make sense of this
 		  in the Ignition environment, backtracing as needed.
+
+		Note that this caches after resolving objects. This is because name references
+		  may be ambiguous or change as the stack mutates.
 		"""
+
+		if ':' in location:
+			location = strip_angle_brackets(location)
+			script_type, _, identifier = location.partition(':')
+
+			if script_type == 'module:':
+				return self._code_module(identifier)
+
+			elif script_type == 'event':
+				component = self.find_root_object('event', frame).source
+				return self._code_event(component, identifier)
+
+			elif script_type == 'tagevent':
+				tag_path = self.find_root_object('tagPath', frame)
+				return self._code_tag_event()
+
+		else:
 
 
 
@@ -37,8 +67,14 @@ class CodeCache(object):
 			return internal_name
 
 
-	def _code_event(component, event_name):
-		event_name = strip_angle_brackets(event_name)
+	@cached
+	def _code_file(cls, filepath):
+		with open(filepath, 'r') as f:
+			return f.read()
+
+
+	@cached
+	def _code_event(cls, component, event_name):
 
 		ic = None
 		ic_comp = component
@@ -57,13 +93,59 @@ class CodeCache(object):
 			return None
 
 
-	def _code_module(filename):
-		filename = strip_angle_brackets(filename)
+	@cached
+	def _code_module(cls, filename):
+		
+		module = sys.modules[filename]
 
-		if filename.startswith('module:'):
-			filename = filename.partition(':')[2]
-			
-		try:
-			return sys.modules[filename].code
-		except:
-			return None
+		if isinstance(module)
+
+		filepath = getattr(module, '__file__', None)
+		if filepath:
+			with open(filepath, 'r') as f:
+				return f.read()
+
+		code = getattr(module, 'code', None)
+		if code:
+			return code
+		
+		return None
+
+
+	@cached
+	def _code_tag_event(cls, tag, event_name):
+		if isinstance(tag, (str, unicode)):
+			tag = system.tag.getTag(tag)
+		return tag.getEventScripts().get(event_name)
+
+
+	@staticmethod
+	def _iter_frames(frame):
+		while frame:
+			yield frame
+			frame = frame.f_back
+
+	@staticmethod
+	def _iter_frame_root(frame):
+		stack = list(_iter_frames(frame))
+		for frame in reversed(stack):
+			yield frame
+
+
+	@staticmethod
+	def find_object(obj_name, frame):
+		"""Grab an item from the Python stack by its name, starting with the given frame."""
+		# if no shortcut is provided, start at the furthest point
+		for frame in _iter_frames(frame):
+			if obj_name in frame.f_locals:
+				return frame.f_locals[obj_name]
+		return None
+
+	@staticmethod
+	def find_root_object(obj_name, frame):
+		"""Grab an item from the Python stack by its name, starting with the given frame."""
+		# if no shortcut is provided, start at the furthest point
+		for frame in _iter_frame_root(frame):
+			if obj_name in frame.f_locals:
+				return frame.f_locals[obj_name]
+		return None
