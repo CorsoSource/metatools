eb07cb2281ad87af311bbd5a47ae590ac613e2b1
Andrew Geiger <andrew.geiger@corsosystems.com>
2020-11-20 03:01:07 -0500

    Sidecar is a simple REST interface dealie. Crowbar uses it to execute   arbitrary code against a gateway. Note that reading gateway   context will likely knock the gateway offline.

---
+++ shared/tools/crowbar.py
@@ -0,0 +1,207 @@
+from shared.tools.pretty import pdir, p, prettify
+from shared.tools.meta import getFunctionCallSigs
+from shared.tools.sidecar import SimpleREST
+from shared.tools.global import ExtraGlobal
+
+from ast import literal_eval
+from cgi import escape
+import re, sys, os
+
+
+from com.inductiveautomation.ignition.gateway import SRContext
+
+
+GLOBAL_CACHE_TIMEOUT = 600
+WHITELIST_GLOBALS = set(['shared', 'system', 'fpmi', 're'])
+WHITELIST_LOCALS = set(['context'])
+
+
+class CrowbarREST(SimpleREST):
+
+
+	def do_GET(self):
+		try:
+			self._do_GET()
+		except Exception, error:
+			system.util.getLogger('Crowbar').error(repr(error))
+			
+			
+	def _do_GET(self):
+		debug = ''
+
+		host, port = self.server.server_address
+		endpoint_url = 'http://%s:%s/' % (host, port)
+
+		#endpoint_url = 'https://localhost.corso.systems:8043/main/system/webdev/Debugger/context/'
+		#endpoint_url = 'https:%s' % (str(request['servletRequest'].httpURI).partition('?')[0],)
+
+		params = self.params
+
+		statement = params.get('eval', '')
+
+		session = ExtraGlobal.setdefault(port, 'Sidecar', {}, lifespan=GLOBAL_CACHE_TIMEOUT)
+
+		session_aliases = session.setdefault('aliases', {})
+		statement_history = session.setdefault('statement_history', [])
+
+		context = session.setdefault('context', SRContext.get())
+
+		while statement in statement_history:
+			statement_history.remove(statement)
+		statement_history.append(statement)
+
+		#	from java.util import Collections
+		#	debug = """Debug:
+		#		|%r
+		#		|%s
+		#		""" % (
+		#			context.getModuleManager().getModules().__class__,
+		#			isinstance(context.getModuleManager().getModules(), Collections),
+		#		)
+
+		try:
+		#		global_scope = globals().copy() # ['app', 'shared', 'system', 'fpmi', 'doGet']
+		#		local_scope = locals().copy() # 	
+		#				['request', 'session_aliases', 'os', 'prettify', 'session', 'pdir', 
+		#				 'session_id', 'global_scope', 'sys', 'params', 'p', 'ExtraGlobal', 
+		#				 'literal_eval', 're', 'endpoint_url', 'statement', 'context', 
+		#				 'getFunctionCallSigs', 'escape']
+
+			global_scope = session.get('global_scope', {})
+			if not global_scope:
+				for var_name in WHITELIST_GLOBALS:
+					global_scope[var_name] = globals()[var_name]
+
+			local_scope = session.get('local_scope', {})
+			if not local_scope:
+				for var_name in WHITELIST_LOCALS:
+					local_scope[var_name] = locals()[var_name]
+
+			local_scope.update(session_aliases)
+			
+			code = compile(statement, '<pretty-eval>', 'eval')
+			result = eval( code , global_scope , local_scope )
+			error = ''
+			if 'alias' in params:
+				session_aliases[params['alias']] = result
+				
+		except Exception, error:
+			# https://stackoverflow.com/a/1278740/11902188
+			exc_type, exc_value, exc_traceback = sys.exc_info()
+			traceback_details = {
+				 'filename': exc_traceback.tb_frame.f_code.co_filename,
+				 'lineno'  : exc_traceback.tb_lineno,
+				 'name'    : exc_traceback.tb_frame.f_code.co_name,
+				 'type'    : exc_type.__name__,
+				 'message' : exc_value.message, # or see traceback._some_str()
+				}
+			del(exc_type, exc_value, exc_traceback) 
+			error = '%r\n\n%s' % (error, p(traceback_details, directPrint=False))
+			result = None
+			show = repr
+
+		if result is None:
+			result = ''
+
+		output_p = '<br>'.join(
+			escape(
+				p(result, ellipsisLimit=120, directPrint=False)
+				.decode('utf8')
+			).splitlines())
+
+		output_pdir = '<br>'.join(
+			escape(
+				pdir(result, ellipsisLimit=120, directPrint=False)
+				.decode('utf8')
+			).splitlines())
+
+
+		AUTO_ATTRIBUTE_PATTERN = re.compile(r'^(\W*)(\w+)(\W+.*)$', re.M)
+		IDENTIFIER_PATTERN = re.compile(r'([a-z0-9A-Z.]+)(@[a-f0-9]+)')
+		CLASS_PATTERN      = re.compile(escape(r'(\<|(com|java|org)\.)([a-z0-9._]+)(\.[a-z0-9_]+)(\>|\.\.\.|)'), re.I)
+
+		auto_attribute_span = r'\1<span style="color:green">\2\3</span>'
+
+		lines = []
+		for line in output_pdir.split('<br>'):
+			m = AUTO_ATTRIBUTE_PATTERN.match(line)
+			if m:
+				whitespace, attribute, remainder = m.groups()
+				if attribute == attribute.upper():
+					line = '<span style="color:gray">%s</span>' % line			
+				elif len(attribute) > 1:
+					attribute = attribute[0].upper() + attribute[1:]
+					if any('%s%s%s' % (whitespace, suffix, attribute) in output_pdir for suffix in 'get is to'.split()):
+						line = AUTO_ATTRIBUTE_PATTERN.sub(auto_attribute_span, line)
+			
+			line = IDENTIFIER_PATTERN.sub(r'\1<span style="color:darkblue">\2</span>', line)
+			line = CLASS_PATTERN.sub(r'<span style="color:goldenrod">\1\3<span style="color:khaki">\4</span>\5</span>', line)	
+			
+			lines.append(line)
+		output_pdir = '<br>'.join(lines)
+			
+		# add easy clicks
+		getter_pattern = re.compile(r'^(\W*)(get)(\w+)', re.M)
+		getter_url = r'\1<span style="color:green">\2</span><a href="%s?eval=%s.\2\3()" style="color:cyan;text-decoration:none">\3</a>' % (endpoint_url, statement)
+		output_pdir = '<br>'.join(
+			getter_pattern.sub(getter_url, line)
+			for line
+			in output_pdir.split('<br>')
+			)
+
+		session_alias_datalist = []
+		for alias in session_aliases:
+			escaped_option = escape(alias.decode('utf8'))
+			session_alias_datalist.append('   <option value="%s">%s</option>' % (escaped_option, escaped_option))
+		session_alias_datalist = '\n'.join(session_alias_datalist)
+
+		statement_history_datalist = []
+		for ix, prev_statement in enumerate(reversed(statement_history)):
+			escaped_option = escape(prev_statement.decode('utf8'))
+			statement_history_datalist.append('   <option value="%s">[%d] %s</option>' % (
+											  escaped_option, len(statement_history) - ix - 1, escaped_option))
+		statement_history_datalist = '\n'.join(statement_history_datalist)
+
+		del result
+		del session_aliases
+		del statement_history
+
+		output = """
+		<html>
+		<head>
+		<meta charset="UTF-8" />
+		</head>
+		<body style="font-family:consolas;font-size:10px; background-color:black; color:lime;">
+		<form action="%(endpoint_url)s"
+		      style="font-family:consolas;font-size:12px; background-color:black; color:yellow;">
+		Statement: 
+			<input type="text" name="eval"  id="eval"  list="statements" value="%(statement)s" autofocus
+			 style="font-family:consolas;font-size:12px; background-color:black; color:yellow; width:700px; height:30px">
+		=&gt;
+			<input type="text" name="alias" id="alias" list="aliases" value=""
+			 style="font-family:consolas;font-size:12px; background-color:black; color:yellow; width:120px; height:30px">
+		<input type="submit" style="visibility: hidden;" />
+		<datalist id="aliases">
+		   %(session_alias_datalist)s
+		</datalist>
+		<datalist id="statements">
+		   %(statement_history_datalist)s
+		</datalist>
+		</form>
+		<div style="font-family:consolas;font-size:10px; background-color:black; color:lightgray;">
+		<pre>%(debug)s</pre>
+		</div>
+		<div style="font-family:consolas;font-size:10px; background-color:black; color:red;">
+		<pre>%(error)s</pre>
+		</div>
+		<div style="font-family:consolas;font-size:10px; background-color:black; color:cyan;">
+		<pre>%(output_p)s</pre>
+		</div>
+		<div style="font-family:consolas;font-size:10px; background-color:black; color:lime; overflow-y: auto; height: 550px">
+		<pre>%(output_pdir)s</pre>
+		</div>
+		</body>
+		</html>
+		""" % locals()
+
+		self.respond_html(output)
---
+++ shared/tools/sidecar.py
@@ -0,0 +1,91 @@
+from shared.tools.thread import async
+from shared.tools.global import ExtraGlobal
+
+import BaseHTTPServer
+from cgi import escape
+import urlparse
+
+
+class SimpleServer(BaseHTTPServer.HTTPServer):
+	allow_reuse_address = True
+	
+	def handle_error(self, request, client_address):
+		system.util.getLogger('Sidecar').error('Error with %r: %r to [%r]' %(self, request, client_address))
+#		request.send_response(400)
+#		request.send_header("Content-type", "text/html")
+#		request.end_headers()
+#		request.wfile.write('Error executing %(command)s for %(client_address)s' % request)
+	
+
+class SimpleREST(BaseHTTPServer.BaseHTTPRequestHandler):
+
+	def respond_html(self, html):
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+		self.wfile.write(html)
+	
+	@staticmethod
+	def html_escape(some_string):
+		return escape(some_string.decode('utf8'))
+	
+	def __getitem__(self, attribute):
+		"""Make this dict-like to simplify things a bit."""
+		try:
+			return getattr(self, attribute)
+		except AttributeError:
+			raise KeyError('%s is not available for this handler')
+	
+	@property
+	def fields(self):
+		try:
+			length  = int(self.headers.getheader('content-length'))
+			field_data = self.rfile.read(length)
+			return urlparse.parse_qs(field_data)
+		except:
+			return None
+			
+	@property
+	def params(self):
+		query_string = urlparse.urlsplit(self.path)[3]
+		params = {}
+		for entry in query_string.split('&'):
+			key,_,value = entry.partition('=')
+			params[key]=value
+		return params
+		
+		
+def shutdown(port):
+	session = ExtraGlobal.setdefault(port, 'Sidecar', {})
+	session['shutdown'] = True
+	
+
+@async(name='Sidecar-REST')
+def launch_sidecar(port, RestHandler):
+	"""
+	This assumes that keep_running() is a function of no arguments which
+	is tested initially and after each request.  If its return value
+	is true, the server continues.
+	"""
+	
+	system.util.getLogger('Sidecar').info("Launching sidecar on port %r with %r" % (port, RestHandler))
+	session = ExtraGlobal.setdefault(port, 'Sidecar', {})
+	
+	server_address = ('localhost', port)
+	httpd = SimpleServer(server_address, RestHandler)
+	try:
+		system.util.getLogger('Sidecar').info("Sidecar started on port %r" % (port,))
+
+		while not ExtraGlobal.get(port, 'Sidecar', {}).get('shutdown', False):
+			httpd.handle_request()
+	except Exception, error:
+		system.util.getLogger('Sidecar').info("Exception on port %r: %r" % (port,error))
+
+	except:
+		pass
+	finally:
+		#print 'Shutting down %r' % (httpd.server_address,)
+		httpd.server_close()
+		ExtraGlobal.trash(port, 'Sidecar') # clear session
+		#print '... done!'
+		system.util.getLogger('Sidecar').info("Sidecar torn down from port %r" % (port,))
