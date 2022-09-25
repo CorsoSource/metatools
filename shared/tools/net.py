a1bfa00f89214ef0add325e74762025d02c3eee2
Andrew Geiger <andrew.geiger@corsosystems.com>
2022-09-25 15:55:34 -0400

    added poke_from_gateway
    Because getting the gateway's perspective is important.

--- shared/tools/net.py
+++ shared/tools/net.py
@@ -77,3 +77,90 @@ def poke(hostname, port, timeout=1.0, from_hostname='', from_port=0):
 		except:
 			pass
 	return True
+
+
+def poke_from_gateway(hostname, ports=tuple(), timeout=1.0,
+		source_tag_provider='default', 
+		poke_module='shared.tools.net', ):
+	"""Ask what the gateway can see. We do this by forcing gateway JVM context via tag script.
+	
+	Returns a dict of ports and True/False for each poke.
+	"""
+	
+	assert isinstance(hostname, str), 'hostname is a string, like "127.0.0.1" or "localhost"'
+	
+	random_test_id = str(uuid.uuid4())	
+
+	if isinstance(ports, int):
+		ports = (ports,)
+	elif isinstance(ports, str):
+		if ',' in ports:
+			ports = tuple(int(port.strip()) for port in ports)
+		else:
+			ports = (int(ports.strip()),)
+			
+	assert all(isinstance(port, int) for port in ports), 'Ports must be numeric, like 80 or "80, 443"'
+	
+	added_folders = []
+	
+	tag_provider = '[%s]' % (source_tag_provider,)
+	temp_tag_folder = 'TEMP/port-tests/%s' % (random_test_id,)
+	base_folders = []
+	base_path_parts = []
+	for name in temp_tag_folder.split('/'):
+		target_base_path = tag_provider + '/'.join(base_path_parts)
+		target_folder_path = tag_provider + '/'.join(base_path_parts + [name])
+		if not system.tag.exists(target_folder_path):
+			system.tag.configure(target_base_path, [{'tagType': 'Folder', 'name': name}])
+			added_folders.append(target_folder_path)
+			
+		base_path_parts.append(name)
+	
+	temp_tag_folder = tag_provider + '/'.join(base_path_parts)
+	
+	tag_configs = []
+	tag_paths = []
+	for port in ports:
+		tag_name = 'port-%d' % (port,)
+		tag_path ='%s/%s' % (temp_tag_folder, tag_name)
+		tag_paths.append(tag_path)
+		
+		tag_config = {
+			'dataType': 'Boolean',
+			'name': tag_name,
+#			'path': tag_path,
+			'tagType': 'AtomicTag',
+			'valueSource': 'memory',
+			'eventScripts': [{
+					'eventid': 'valueChanged',
+					'script': """
+	from %(poke_module)s import poke
+	
+	result = poke(%(hostname)r, %(port)r, %(timeout)r)
+	
+	system.tag.writeBlocking([tagPath], [result])
+	""" % locals(),
+			}],		
+		}
+		
+		tag_configs.append(tag_config)
+	
+	print repr(tag_config)
+	
+	system.tag.configure(temp_tag_folder, tag_configs)
+
+	sleep(timeout + 0.5)
+	
+	results = [tvq.value for tvq in system.tag.readAll(tag_paths)]
+	
+	# clean up mess
+	for folder_path in reversed(sorted(added_folders)):
+		system.tag.deleteTags([folder_path])
+	
+	return dict(
+		(port, result)
+		for port, result
+		in zip(ports, results)
+	)
+
+#poke_from_gateway('127.0.0.1', [80, 443, 8088, 8089, 8043, 8060])
