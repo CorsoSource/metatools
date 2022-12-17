f829de319e69338172ba6c7c12123bb4e3439925
Andrew Geiger <andrew.geiger@corsosystems.com>
2022-12-16 21:42:52 -0500

    Add sigfigs and combination helpers. And clock drift monitoring (#11)
    * Add sigfigs and combination helpers
    
    `round_sigfigs` makes humanizing values a bit easier (or at least staying consistent and properly propagating rounding errors)
    
    Also added is `combine_stats` which makes it a easier to accumulate summaries of statistical populations that accumulate in sample groups over time.
    
    * Update stats.py
    
    combined didn't return thing
    
    * clean up args
    
    * Refactored histogram for better control
    
    because i wanted "start at x and go every y for n"
    
    * async logs are bunk
    
    added at least the name of the calling context because line number and context is annoying
    
    * Create monitor.py
    
    Added the first of the system monitoring scripts, `clock_drift_monitor`. This tracks how much the system seems to typically behave in a statistical way. (Panopticon2 is for correlating against actual events)
    
    * added way to inject monitor into gateway scope
    
    simply call `spawn_clock_drift_monitor`
    
    * Update monitor.py
    
    Corrected path to use the_gateway_'s idea of the log path (and not the designer's 🤦 )

--- shared/data/stats.py
+++ shared/data/stats.py
@@ -12,6 +12,50 @@ class StatisticsError(ValueError): pass # Generic "this won't work" error for st
 class InsufficientData(StatisticsError): pass # Generic "need more numbers" error
 
 
+
+def magnitude(x):
+	"""https://stackoverflow.com/a/16839304"""
+	return int(math.floor(math.log10(x)))
+
+
+def order_of_magnitude(x):
+	"""Returns the base-10 order of magnitude for a value. 
+	So 12.3 is 10, 0.03 is 0.01
+	"""
+	return 10**magnitude(x)
+
+
+def mag_floor(x, oom):
+	"""Returns the floor of x snapping to the given order of magnitude. 
+	So 12.345 given 0.01 is 12.34, -5.67 at 0.1 is -5.7 
+	"""
+	return x // oom * oom
+
+
+def mag_ceil(x, oom):
+	"""Returns the ceiling of x snapping to the given order of magnitude. 
+	So 12.345 at 0.1 is 12.4, -5.67 at 0.1 is -5.6"""
+	q,r = divmod(x, oom)
+	q *= oom
+	if r:
+		q+=oom
+	return q
+
+
+def round_sigfigs(x, n=None):
+	"""Round x to n digits significant figures. Defaults to x (no sigfigs)."""
+	if n is None:
+		return x
+	assert n >= 1
+	oom = 10**(magnitude(x)-(n-1))
+	q,r = divmod(x, oom)
+	q *= oom
+	if r/oom > 0.5:
+		q += oom
+	return q
+
+
+
 def prime_on_first(iterator):
 	x = None
 	try:
@@ -97,7 +141,6 @@ def geometric_mean(iterable):
 	return negative_correction_compensation * math.exp((1.0/len(values))*sum_logs)
 
 
-
 def harmonic_mean(iterable):
 	"""Scans over the iterable and returns the reciprocal of the mean of the recipricals of non-None values
 	If a value is 0, the result is zero
@@ -145,6 +188,7 @@ def standard_deviation(iterable):
 	return math.sqrt(variance(iterable))
 
 stdev = standard_deviation
+
 
 def median(iterable):
 	"""Returns the value in the middle of the iterable (after filtering out None)"""
@@ -230,40 +274,12 @@ def multimode(iterable, truncation_magnitude=None):
 def mode(iterable, truncation_magnitude=None):
 	"""Returns the most common value (or one of from the set of them, ignoring None)"""
 	return next(iter(multimode(iterable, truncation_magnitude)))
-	
-
-def magnitude(x):
-	"""https://stackoverflow.com/a/16839304"""
-	return int(math.floor(math.log10(x)))
-
-
-def order_of_magnitude(x):
-	"""Returns the base-10 order of magnitude for a value. 
-	So 12.3 is 10, 0.03 is 0.01
-	"""
-	return 10**magnitude(x)
-
-
-def mag_floor(x, oom):
-	"""Returns the floor of x snapping to the given order of magnitude. 
-	So 12.345 given 0.01 is 12.34, -5.67 at 0.1 is -5.7 
-	"""
-	return x // oom * oom
-
-
-def mag_ceil(x, oom):
-	"""Returns the ceiling of x snapping to the given order of magnitude. 
-	So 12.345 at 0.1 is 12.4, -5.67 at 0.1 is -5.6"""
-	q,r = divmod(x, oom)
-	q *= oom
-	if r:
-		q+=oom
-	return q
 
 
 
-def histogram(iterable, buckets=20):
+def histogram(iterable, buckets=None, start=None, stop=None, step=None):
 	"""Returns a list of counts for entries in iterable that fit in evenly spaced buckets."""
+	
 	iterator = iter(iterable)
 	x = prime_on_first(iterator)
 	
@@ -279,54 +295,118 @@ def histogram(iterable, buckets=20):
 			minx = x
 		if x > maxx:
 			maxx = x
-
+			
+	x_start = start or minx
+	x_end = stop or maxx
+		
+	x_span = x_end - x_start
 	
-	# snap to order of magnitude	
-	oom = order_of_magnitude((maxx - minx) / buckets)
+#	print x_start, x_end, x_span
+			
+	# resolve and snap to order of magnitude for unconstrained cases
+	if buckets:
+		oom = order_of_magnitude(x_span / buckets)
+	elif step:
+		oom = order_of_magnitude(step)
+	else:
+		oom = order_of_magnitude(10**round(math.log10(x_span))/10.0)
 	
-	minx = mag_floor(minx, oom)
-	maxx = mag_ceil(maxx, oom)
+	# re-resolve unset things given the order of magnitude
+	if not start:
+		x_start = mag_floor(x_start, oom)
+	if not stop:
+		x_end = mag_ceil(x_end, oom)
+		
+	if not (step or buckets):
+		x_step = oom
+		x_buckets = int(math.ceil((x_end - x_start)/float(x_step)))
+	# buckets win in a tie with step since 
+	# evenly spaced buckets are better than consistent steps
+	# (that's a hot take, but buckets define histogram length, which is kind of a contract)
+	elif buckets: 
+		x_buckets = int(math.ceil(buckets))
+		x_step = ((x_end - x_start) / float(buckets))
+	else: # step:
+		x_step = step
+		x_buckets = int(math.ceil((x_end - x_start) / float(step)))
 	
-	width = (maxx - minx) / buckets
-	
-	counts = [0] * buckets
+	counts = [0] * x_buckets
+	dropped = 0
 	for v in values:
-		v -= minx
-		counts[int(v // width)] += 1
+		v -= x_start
+		try:
+			counts[int(v // x_step)] += 1
+		except:
+			dropped += 1
 	
-	return counts
-	
-	
-	
-def format_histogram(values, height=5, width=40):
+#	p((slice(x_start, x_end, x_step), counts), nestedListLimit=None)
+	return slice(x_start, x_end, x_step), counts
+
+
+def format_histogram(values, height=5, buckets=None, start=None, stop=None, step=None):
 	"""Returns a text-based simple histogram."""
-	counts = histogram(values, width)
+	if height < 3:
+		height = 3
 	
+	# config is a slice of the actual start, stop, and step of the histogram
+	config, counts = histogram(values, buckets, start, stop, step)
+	buckets = len(counts)
+	
+	# calculate plot verical lmits
 	height = float(height)
-	
 	maxc = max(counts)
 	oom = order_of_magnitude(maxc / height)
-	
-	left_margin = len(str(maxc)) + 1
+
+	top = maxc
+	v_step = mag_ceil(maxc, oom) / height
+
+
+	# calculate how the margin works (get the longest string that can show up there)
+	left_margin = max(map(lambda x: len(str(x)), 
+					  [maxc, config.start, round_sigfigs(v_step, 4)+1,])
+					  ) + 1
 	margin_pad = '  '
 	left = '%%%ds' % (left_margin,) + margin_pad
 	marker = '+'
-	
-	top = maxc
-	step = mag_ceil(maxc, oom) / height
+
 	
 	plot_lines = []
 	for i in range(int(height))[:-1]:
-		top -= step
+		top -= v_step
 		
-		plot_lines.append( (left % (maxc if not i else '')) + ''.join(marker if c > top else ' ' for c in counts) )
+		if i == 0:
+			left_text = maxc
+		elif i == int(height//2):
+			left_text = (u'Δ' + str(round_sigfigs(v_step, 4)))
+		else:
+			left_text = ''
+		
+		plot_lines.append( (left % left_text) + ''.join(marker if c > top else ' ' for c in counts) )
 	# floating point precision errors mean maxc is probably not 0
 	# so we'll just fluff the last line here
 	plot_lines.append( (left % 0) + ''.join(marker if c else ' ' for c in counts) )
-	plot_lines.append( ('-' * left_margin + margin_pad) + '=' * width )
-	plot_lines.append( (left % mag_floor(min(values),oom)) + ' ' * width + (str(mag_ceil(max(values), oom))) )
+	plot_lines.append( ('-' * left_margin + margin_pad) + '=' * buckets )
+	plot_lines.append( (left % (config.start) + ((u'Δ%%-%ds' % (buckets-1)) % round_sigfigs(config.step, 5)) + str( config.stop )) )
 	
 	return '\n'.join(plot_lines)
+
+
+
+def combine_stats(n1,mean1,variance1, n2,mean2,variance2):
+	"""Merge two sample groups together.
+	ref https://math.stackexchange.com/a/2971563
+	"""
+	new_n = n1 + n2
+	new_mean = ((n1*mean1) + (n2*mean2))/(n1+n2)
+	new_variance = (
+		(   ( ((n1-1)*variance1) + ((n2-1)*variance2) )
+		  / ( n1 + n2 - 1.0                           )
+		) + (
+		    ( (n1*n2)*pow(mean1-mean2,2) )
+		  / ( (n1+n2)*(n1+n2-1.0)        )
+		)
+	)
+	return new_n, new_mean, new_variance
 
 
 
@@ -373,6 +453,59 @@ def describe(iterable):
 		'span': maxx - minx,
 	}
 
+
+
+def combine_descriptions(desc1, desc2):
+	d3_n, d3_mean, d3_variance = combine_stats(
+		desc1['n'],desc1['mean'],desc1['variance'],
+		desc2['n'],desc2['mean'],desc2['variance'],
+		)
+
+	desc3 = {
+		'n': d3_n,
+		'min': min((desc1['min'], desc2['min'])),
+		'max': max((desc1['max'], desc2['max'])),
+		'sum': desc1['sum'] + desc2['sum'],
+		'mean': d3_mean,
+		'variance': d3_variance,
+		'standard deviation': math.sqrt(d3_variance),
+	}
+	
+	desc3['span']  = desc3['max'] - desc3['min']
+	
+	return desc3
+
+
+
+NON_ROUNDING_DESCRIPTION_KEYS = set(['n','sum'])
+
+
+def apply_rounding(x, sigfigs=None, round_digits=None, skip_keys=NON_ROUNDING_DESCRIPTION_KEYS):
+	"""Applies a rounding/sigfig coersion to x. 
+	If x is an iterable, it will be traversed and recursively applied as well.
+	"""
+	if isinstance(x, (int, float)):
+		x = round_sigfigs(x, sigfigs)
+		if round_digits:
+			return round(x, round_digits)
+		else:
+			return x
+	if isinstance(x, (list, tuple, set)):
+		return type(x)(
+			apply_rounding(v, sigfigs, round_digits)
+			for v in x)
+	if isinstance(x, dict):
+		return dict(
+			(k, x[k] 
+			    if k in skip_keys 
+			    else apply_rounding(x[k], sigfigs, round_digits) )
+			for k in x
+			)
+	else: # assume some sort of other iterable
+		return (apply_rounding(v, sigfigs, round_digits) for v in x)
+	return apply_rounding(desc3, sigfigs, round_digits)
+
+
 #
 #data = [
 #	4,3,1,2,2,None,None,2,1,1,3,3,5,None,6,1.5,3,4,3.33,1,2,2,1
@@ -397,6 +530,4 @@ def describe(iterable):
 #assert round(harmonic_mean([40, 60]),1) == 48.0
 #assert round(harmonic_mean([2.5, 3, 10]),1) == 3.6
 #assert round(geometric_mean([54, 24, 36]), 1) == 36.0
-
-
 
