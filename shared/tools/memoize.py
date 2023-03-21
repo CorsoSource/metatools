c60f9d61e93e04e379dc3ad24b775f644f6026a3
Andrew Geiger <andrew.geiger@corsosystems.com>
2023-03-20 23:29:37 -0400

    Update memoize.py
    Changed memoize so that it hashes the default values with the args, or with the kwarg overrides. It's not that the `arg_hash` was bad, but a `frozenset` of a `dict.items()` just isn't as good as a plain ol' arg `tuple`.

--- shared/tools/memoize.py
+++ shared/tools/memoize.py
@@ -1,39 +1,55 @@
 """
-    Memoize functions - but only within the same function call
-
-from time import sleep
-
+    Memoize functions
     
-@memoize_for_call_stack
-def do_thing(x, y=3):
-print '<< direct call: ', x, y
-return x + y
+    memoize                - remember results for any particular (hashable) inputs
+    memoize_for_call_stack - same, but only within the same function call
+                             (this is a uniquely Jython appropriate thing,
+                              since one thread may execute many Python contexts)
 
-
-print '=== no defaults ==='
-do_thing(10)
-do_thing(10)
-print '=== all args ==='
-do_thing(10,6)
-do_thing(10,6)
-print '=== args kwargs ==='
-do_thing(10, y=34)
-do_thing(10, y=34)
-print '=== all kwargs ==='
-do_thing(x=2, y=10)
-do_thing(x=2, y=10)
-
-
-print 'testing timeout'
-sleep(11)
-print 'after timeout'
-do_thing(10)
-print 'testing failsafe'
-do_thing([1,2],[3,4])
 """
+
 import sys
 from datetime import datetime, timedelta
 from functools import wraps
+
+
+def memoize(function):
+    """Memoize outputs.
+    
+    Note that this does _not_ expire the cache, so switch to the 
+    shared.tools.cache decorator if occasional culling is needed.
+    """
+    @wraps(function)
+    def wrapped_function(*args, **kwargs):
+        # convert kwargs to args (with defaults), if any, since we can't key on a dict
+        if kwargs:
+            key_args = args + tuple( # default or kwargs
+                kwargs.get(arg_name, default)
+                for default, arg_name
+                in zip(
+                    function.func_defaults,
+                    function.func_code.co_varnames[len(args):function.func_code.co_argcount],
+                    )
+                )
+        else:
+            key_args = args
+        
+        try: # to generate the hash of the args
+            memo_key = hash(key_args)
+        # failsafe and simply pass to function
+        except TypeError:
+            return function(*args, **kwargs)
+        
+        # if not memo_key in cache, calculate and add it
+        if not memo_key in wrapped_function.memo_cache:
+            value = function(*args, **kwargs)
+            wrapped_function.memo_cache[memo_key] = value
+        
+        return wrapped_function.memo_cache[key_args]
+        
+    wrapped_function.memo_cache = {}
+    
+    return wrapped_function
 
 
 
@@ -46,48 +62,6 @@ def root_stack_frame(max_depth = 100):
             return frame
     else:
         raise RuntimeError('Failed to find root stack')
-
-
-def arg_hash(args, kwargs):
-    return hash((args, frozenset(kwargs.items())))
-    
-
-def memoize(function):
-    """Memoize outputs.
-    
-    Note that this does _not_ expire the cache, so switch to the 
-    shared.tools.cache decorator if occasional culling is needed.
-    """
-    @wraps(function)
-    def memoized(*args, **kwargs):
-        
-        # in the event something unhashable was sent, this'll failsafe      
-        key_state = 0
-        try:
-            memo_key = arg_hash(args, kwargs)
-            if memo_key not in memoized.cache:
-                key_state = 1
-            else:
-                key_state = 2
-        except TypeError:
-            key_state = 0
-        
-        # failsafe or cache the result if needed...
-        if key_state == 0:
-            # failsafe and simply run the function
-            print 'cache fail'
-            return function(*args, **kwargs)
-        elif key_state == 1:
-            # cache the fresh entry
-            print 'adding cache'
-            memoized.cache[memo_key] = function(*args, **kwargs)
-        # ... and return the result
-        return memoized.cache[memo_key]
-    
-    memoized.cache = {}
-    
-    return memoized
-
 
 
 #CALLSTACK_CACHE_EXPIRATION = timedelta(minutes=10)
@@ -147,3 +121,34 @@ def memoize_for_call_stack(function):
     memoized_call.timeout = {}
     
     return memoized_call
+
+
+
+
+def _run_example():
+    from time import sleep
+
+    @memoize_for_call_stack
+    def do_thing(x, y=3):
+        print '<< direct call: ', x, y
+        return x + y
+
+    print '=== no defaults ==='
+    do_thing(10)
+    do_thing(10)
+    print '=== all args ==='
+    do_thing(10,6)
+    do_thing(10,6)
+    print '=== args kwargs ==='
+    do_thing(10, y=34)
+    do_thing(10, y=34)
+    print '=== all kwargs ==='
+    do_thing(x=2, y=10)
+    do_thing(x=2, y=10)
+
+    print 'testing timeout'
+    sleep(11)
+    print 'after timeout'
+    do_thing(10)
+    print 'testing failsafe'
+    do_thing([1,2],[3,4])
